934cfb1550f75550df57ebe9048df9f04f555868
Matheus Duda <105324664+Matheus-Sanchez@users.noreply.github.com>
2023-05-08 18:19:13 -0300

    tentando exibir tudo

---
+++ Cartagena-visual-do-jogo/Sistema Autonomo/Estrategia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Autonomo
+{
+    internal class Estrategia
+    {
+        Jogador player;
+            Tabuleiro tabuleiro;
+        
+
+        /*public Estrategia (Jogador jogador, Tabuleiro tabuleiro, int ID, string Senha, string Cor, List<string> tabuleiro, List<Pirata> piratas)
+        {
+            this.jogador = player;
+            this.tabuleiro = tabuleiro;
+            
+
+
+        }*/
+
+        public void JogarSemEstrategia()
+        {
+
+        }
+    }
+}
